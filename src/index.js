a48757faac4a2129e5d8842b397c6a3f5251185d
Future Automation LLC <61432405+mmarshalleet@users.noreply.github.com>
2026-04-17 08:16:32 -0500

    Add product preview data to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-
+{
+  "preview": true,
+  "rows": [
+    {
+      "sku": "2711R-T10T",
+      "photoUrls": [
+        "https://i.ebayimg.com/images/g/abc/s-l1600.jpg",
+        "https://i.ebayimg.com/images/g/def/s-l1600.jpg"
+      ],
+      "brand": "Allen-Bradley",
+      "mpn": "2711R-T10T",
+      "model": "PanelView 800",
+      "type": "HMI",
+      "condition": "New",
+      "keywords": ["10 in", "Touch", "Factory Sealed"]
+    }
+  ]
+}
